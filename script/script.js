ecc04ef553553efdc67ea6116ee350e753816a23
tanvir-hadan-code <tanvirx015@gmail.com>
2025-03-17 01:25:21 +0600

    btn add

--- script/script.js
+++ script/script.js
@@ -11,13 +11,13 @@ document
     const convertPassword = parseInt(userPassword);
     if (userName) {
       if (userPassword) {
-		  if (convertPassword === 123456) {
-              Swal.fire({
-                icon: "success",
-                title: "অভিনন্দন",
-                text: "চলুন আজ নতুন কিছু শেখা জাক",
-                confirmButtonText: "OK",
-              });
+        if (convertPassword === 123456) {
+          Swal.fire({
+            icon: "success",
+            title: "অভিনন্দন",
+            text: "চলুন আজ নতুন কিছু শেখা জাক",
+            confirmButtonText: "OK",
+          });
           document.getElementById("bannerSection").style.display = "none";
           document.getElementById("navber").style.display = "flex";
           document.getElementById("learnSection").style.display = "grid";
@@ -36,52 +36,72 @@ document
 //   logout btn
 
 document.getElementById("btn-logout").addEventListener("click", function () {
-	document.getElementById("bannerSection").style.display = "flex";
+  document.getElementById("bannerSection").style.display = "flex";
   document.getElementById("navber").style.display = "none";
   document.getElementById("learnSection").style.display = "none";
   document.getElementById("faqSection").style.display = "none";
 });
 // learn section
-document.getElementById('lesson0').style.display = 'none';
-document.getElementById('lesson0').style.display = 'none';
+document.getElementById("lesson0").style.display = "none";
+document.getElementById("lesson0").style.display = "none";
+
+// Dynamic btn name
+
+fetch("https://openapi.programming-hero.com/api/levels/all")
+  .then((res) => res.json())
+  .then((response) => {
+    const data = response.data;
+
+    document.querySelectorAll(".click-btn").forEach((button, index) => {
+      if (index < data.length) {
+        button.querySelector(".text-btn").innerText =
+          `lesson-` + data[index].level_no;
+      }
+    });
+  })
+  .catch((error) => console.error("Error fetching data:", error));
 
 // lesson -1
-document.getElementById('lesson-1').addEventListener('click',
-	function () {
-		
-	}
-)
+document.getElementById("lesson-1").addEventListener("click", function () {
+  fetch("https://openapi.programming-hero.com/api/level/5")
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
 
 // lesson -3
-document.getElementById('lesson-3').addEventListener('click',
-	function () {
-		document.getElementById('child-learn').style.display = 'none';
-		document.getElementById('lesson0').style.display = 'grid';
-
-	}
-)
+document.getElementById("lesson-3").addEventListener("click", function () {
+  document.getElementById("child-learn").style.display = "none";
+  document.getElementById("lesson0").style.display = "grid";
+});
 
 // lesson 7
-document.getElementById('lesson-7').addEventListener('click',
-	function () {
-		document.getElementById('child-learn').style.display = 'none';
-		document.getElementById('lesson0').style.display = 'grid';
-
-	}
-)
-
+document.getElementById("lesson-7").addEventListener("click", function () {
+  document.getElementById("child-learn").style.display = "none";
+  document.getElementById("lesson0").style.display = "grid";
+});
 
 // learn btn
 
 document.getElementById("btn-learn").addEventListener("click", function () {
   document
-	.getElementById("learnSection")
-	.scrollIntoView({ behavior: "smooth" });
+    .getElementById("learnSection")
+    .scrollIntoView({ behavior: "smooth" });
 });
 
 // faq btn
 
 document.getElementById("btn-faq").addEventListener("click", function () {
-  document.getElementById("faqSection").scrollIntoView({ behavior: "smooth"});
+  document.getElementById("faqSection").scrollIntoView({ behavior: "smooth" });
 });
 
+// btn active
+
+function btnActive(click) {
+  document.querySelectorAll(".click-btn").forEach((button) => {
+    button.classList.remove("btn-primary");
+    button.classList.add("btn-outline");
+  });
+
+  click.classList.add("btn-primary");
+  click.classList.remove("btn-outline");
+}
